a5662b58314456b91039eaccd164f9312f2af314
Egor <efsvags@icloud.com>
2021-02-20 22:28:30 +0300

    Adding cart functionality

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Categories } from "./categories";
 export { default as Sort } from "./sort";
 export { default as PizzaBlock } from "./pizza-block";
 export { default as LoadingBlock } from "./loading-block";
+export { default as CartItem } from "./cart-item";
